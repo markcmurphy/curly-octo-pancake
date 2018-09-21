cf43cf27a064170944a6aea57a13a2cb6dc9b303
Mark Murphy <mark@murphymark.me>
2018-09-21 08:51:42 -0500

    updated server.js with process.env.port to run on Heroku

--- server.js
+++ server.js
@@ -3,24 +3,25 @@ const app = express();
 const bunch = require('express-bunch-middleware');
 const getAirportData = require('./bin/airportData.js');
 const getAirportWeather = require('./bin/airportWeather.js');
+const port = process.env.PORT || 3000;
 
 // express-bunch-middleware
 app.use('/resources', bunch);
 
 app.get('/dataByAirport/:airport_identifier', (req, res) => {
-console.log(req.params, 'req.params');
-getAirportData(res, req.params.airport_identifier)
+  console.log(req.params, 'req.params');
+  getAirportData(res, req.params.airport_identifier)
 });
 
 app.get('/weatherByAirport/:airport_identifier', (req, res) => {
   console.log(req.params, 'req.params');
   setTimeout(() => {
-		getAirportWeather(res, req.params.airport_identifier)
-    }, 30)
-	});
+    getAirportWeather(res, req.params.airport_identifier)
+  }, 30)
+});
 
 // end of express-bunch-middleware
 
-app.listen(3000, () => {
-  console.log('listening');
+app.listen(port, () => {
+  console.log('App running on port' + port);
 });
